40da515c0adb2b1fb26a3da71e364b51aad71b6b
Frankee.zhou <frankee.zhou@wayz.ai>
2021-12-28 09:21:36 +0800

    add a basic filter module for list method

---
+++ pkg/filter/http_query_filter_test.go
@@ -0,0 +1,20 @@
+package filter
+
+import (
+	"github.com/alecthomas/assert"
+	"net/url"
+	"testing"
+)
+
+func TestHttpQueryFilter_Compile(t *testing.T) {
+	query := url.Values{
+		"foo":  []string{`"bar"`},
+		"far":  []string{"in(a, b, c)"},
+		"name": []string{`"*bb"`},
+		"count": []string{"1..<6"},
+		"filter": []string{"a > 0 && a < 100"},
+	}
+
+	filter := HttpQueryFilter{}.Compile(query)
+	assert.NotEmpty(t, filter)
+}
